a38e2078730097e588cce68cfb3c2b378418889f
WillWare <will@willware.tv>
2020-08-22 23:43:40 -0500

    Trying SAFEMUL32 on the RNG results

--- lua/5.3/mt/mt19937ar.lua
+++ lua/5.3/mt/mt19937ar.lua
@@ -275,7 +275,7 @@ function mt19937ar.new()
 		y = XOR(y, AND(SHL7(y), 0x9D2C5680) )
 		y = XOR(y, AND(SHL15(y), 0xEFC60000) )
 		y = XOR(y, SHR18(y))
-		return y / 2
+		return y
 	end
 
 	-- Floating Point Versions
@@ -287,24 +287,24 @@ function mt19937ar.new()
 
 	--- generates a random number on [0,1]-real-interval
 	function instance:genrand_real1()
-		return self:genrand_int32() * (1.0/4294967295.0) -- divided by 2^32-1
+		return SAFEMUL32(self:genrand_int32(), (1.0/4294967295.0)) -- divided by 2^32-1
 	end
 
 	--- generates a random number on [0,1)-real-interval
 	function instance:genrand_real2()
-		return self:genrand_int32() * (1.0/4294967296.0) -- divided by 2^32
+		return SAFEMUL32(self:genrand_int32(), (1.0/4294967296.0)) -- divided by 2^32
 	end
 
 	--- generates a random number on (0,1)-real-interval
 	function instance:genrand_real3()
-		return (self:genrand_int32() + 0.5) * (1.0/4294967296.0) -- divided by 2^32
+		return SAFEMUL32((self:genrand_int32() + 0.5), (1.0/4294967296.0)) -- divided by 2^32
 	end
 
 	--- generates a random number on [0,1) with 53-bit resolution
 	function instance:genrand_res53()
 		local a = floor(self:genrand_int32() / 32)
 		local b = floor(self:genrand_int32() / 64)
-			return (a*67108864.0+b) * (1.0/9007199254740992.0)
+			return SAFEMUL32((a*67108864.0+b), (1.0/9007199254740992.0))
 	end
 	--/* These real versions are due to Isaku Wada, 2002/01/09 added */
 
