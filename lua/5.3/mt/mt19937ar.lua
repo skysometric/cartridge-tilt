8de4734150a9ac2592cb435e0b40d0f098a63861
WillWare <will@willware.tv>
2020-08-22 23:47:34 -0500

    Try adjusting genrand_real functions to 2^31

--- lua/5.3/mt/mt19937ar.lua
+++ lua/5.3/mt/mt19937ar.lua
@@ -287,24 +287,24 @@ function mt19937ar.new()
 
 	--- generates a random number on [0,1]-real-interval
 	function instance:genrand_real1()
-		return SAFEMUL32(self:genrand_int32(), (1.0/4294967295.0)) -- divided by 2^32-1
+		return self:genrand_int32() * (1.0/2147483647.0) -- divided by 2^31-1
 	end
 
 	--- generates a random number on [0,1)-real-interval
 	function instance:genrand_real2()
-		return SAFEMUL32(self:genrand_int32(), (1.0/4294967296.0)) -- divided by 2^32
+		return self:genrand_int32() * (1.0/2147483648.0) -- divided by 2^31
 	end
 
 	--- generates a random number on (0,1)-real-interval
 	function instance:genrand_real3()
-		return SAFEMUL32((self:genrand_int32() + 0.5), (1.0/4294967296.0)) -- divided by 2^32
+		return (self:genrand_int32() + 0.5) * (1.0/2147483648.0) -- divided by 2^31
 	end
 
 	--- generates a random number on [0,1) with 53-bit resolution
 	function instance:genrand_res53()
 		local a = floor(self:genrand_int32() / 32)
 		local b = floor(self:genrand_int32() / 64)
-			return SAFEMUL32((a*67108864.0+b), (1.0/9007199254740992.0))
+			return (a*67108864.0+b) * (1.0/9007199254740992.0)
 	end
 	--/* These real versions are due to Isaku Wada, 2002/01/09 added */
 
